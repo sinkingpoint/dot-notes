c05fb710f8d355cbe5bb990244add8a3cca06442
sinkingpoint <colin@quirl.co.nz>
2020-12-27 18:51:44 +0000

    Add front page to backend

--- backend/src/routes/statics/statics_standalone.rs
+++ backend/src/routes/statics/statics_standalone.rs
@@ -3,7 +3,7 @@ use warp::reply::Response;
 use warp::http::header;
 
 static EDITOR_BUNDLE: &[u8] = include_bytes!("../../../bundles/note_editor_page-bundle.js");
-static CONFIG_BUNDLE: &[u8] = include_bytes!("../../../bundles/note_schedule_page-bundle.js");
+static FRONT_PAGE_BUNDLE: &[u8] = include_bytes!("../../../bundles/main_page-bundle.js");
 
 // In Prod mode, we bundle the frontend into the binary (using the `include_bytes` above)
 // so we need to manually specify each "file"
@@ -16,13 +16,13 @@ pub fn get_routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rej
             resp
         });
     
-    let get_config_bundle_path = warp::path!("dist" / "config_page-bundle.js")
+    let get_front_page_bundle_path = warp::path!("dist" / "main_page-bundle.js")
         .and(warp::get())
         .map(|| {
-            let mut resp = Response::new(CONFIG_BUNDLE.into());
+            let mut resp = Response::new(FRONT_PAGE_BUNDLE.into());
             resp.headers_mut().insert(header::CONTENT_TYPE, header::HeaderValue::from_static("application/javascript"));
             resp
         });
     
-    return get_editor_bundle_path.or(get_config_bundle_path).with(warp::log("standalone"));
+    return get_editor_bundle_path.or(get_front_page_bundle_path).with(warp::log("standalone"));
 }
